1e417ddf5635320c921025ffd5219aa754478e69
lyklecharova <lklecharova9@gmail.com>
2026-01-16 18:48:02 +0200

    Add movie selection flow with list and details view

--- App.js
+++ App.js
@@ -1,38 +1,35 @@
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import data from './data.json';
-import MovieCard from './components/MovieCard';
+import { useState } from 'react';
+import MovieList from './components/MovieList';
+import MovieDetails from './components/MovieDetails';
 
 export default function App() {
-    
+    const [selectedMovie, setSelectedMovie] = useState(null);
+    console.log(selectedMovie);
+
     const moviePressHandler = (movieId) => {
-        alert(`Movie Pressed: ${movieId}`);
+        const movie = data.movies.find((movie) => movie.id === movieId);
+        setSelectedMovie(movie);
+    };
+
+    const closeDetailsHandler = () => {
+        setSelectedMovie(null);
     };
 
     return (
         <View style={{ paddingTop: 50, backgroundColor: '#faf0e6' }}>
-            <Text
-                style={{
-                    textAlign: 'center',
-                    fontSize: 30,
-                    fontWeight: 'bold',
-                    marginBottom: 20,
-                }}
-            >
-                Movie Catalog
-            </Text>
-
-            <View>
-                {data.movies.map((movie) => (
-                    <MovieCard
-                        key={movie.id}
-                        id={movie.id}
-                        title={movie.title}
-                        year={movie.year}
-                        imageUrl={movie.imageUrl}
-                        onPress={moviePressHandler}
-                    />
-                ))}
-            </View>
+            {selectedMovie ? (
+                <MovieDetails
+                    movie={selectedMovie}
+                    onClose={closeDetailsHandler}
+                />
+            ) : (
+                <MovieList
+                    movies={data.movies}
+                    moviePressHandler={moviePressHandler}
+                />
+            )}
         </View>
     );
 }
