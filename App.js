a361ed6b0566f085c0dfde34e810e232349b0053
lyklecharova <lklecharova9@gmail.com>
2026-01-16 13:10:38 +0200

    Render movie catalog in App component

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { Text, View } from 'react-native';
+import data from './data.json';
+import MovieCard from './components/MovieCard';
 
 export default function App() {
     return (
         <View style={{ marginTop: 50 }}>
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text
+                style={{
+                    textAlign: 'center',
+                    fontSize: 30,
+                    fontWeight: 'bold',
+                }}
+            >
+                Movie Catalog
+            </Text>
+
+            <View>
+                {data.movies.map((movie) => (
+                    <MovieCard key={movie.id} title={movie.title} />
+                ))}
+            </View>
         </View>
     );
 }
